9b1bb4ef01348eb9f39c5a59337ad388a72dec5d
Ocelyn <ocelyndehmie24@gmail.com>
2023-02-16 13:38:33 -0500

    intial commit

---
+++ app/src/main/java/com/example/tipcal/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tipcal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Amount;
+    private TextView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
